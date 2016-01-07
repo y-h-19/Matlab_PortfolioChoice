c67f96942ac1ca4c00e77ef6f0ac8806755f5204
y-hahn <ahahn2@wellesley.edu>
2016-01-07 20:51:53 +0000

    Create undoFunction.m

---
+++ undoFunction.m
@@ -0,0 +1,26 @@
+% The undoFunction undoes the result values and the result text when the
+% 'Undo' button is pressed
+
+% Narayani G., Yuna Hahn
+% CS 112 Project: Portfolio Choice
+% Spring 2014
+
+function handles = undoFunction(handles)
+
+% setting result values for the relevant graph to an empty string when
+% undo is pressed
+if handles.where(end) == 1
+    set(handles.ce1, 'string', '');
+    set(handles.pi1, 'string', '');
+    set(handles.ra1, 'string', '');
+    set(handles.rr1, 'string', '');
+elseif handles.where(end) == 2
+    set(handles.ce2, 'string', '');
+    set(handles.pi2, 'string', '');
+    set(handles.ra2, 'string', '');
+    set(handles.rr2, 'string', '');
+end
+
+% setting textBox to an empty string when undo is pressed
+set(handles.textBox, 'string', '');
+
